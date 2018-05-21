c3e069fd3fcf72c1c91311515754c11f88b07d25
DmytroKaminskyi <dmytrok@completecase.com>
2018-05-21 23:17:03 +0300

    small config refactor

--- app/factories.py
+++ app/factories.py
@@ -5,7 +5,8 @@ from flask_assets import Bundle
 from raven.contrib.celery import register_signal, register_logger_signal
 
 from app import extensions as ext, error_handlers
-from configs import Config
+
+CONFIG_MODULE = 'configs'
 
 
 def create_celery(app, celery):
@@ -18,6 +19,7 @@ def create_celery(app, celery):
                 return self.run(*args, **kwargs)
 
     celery.Task = ContextTask
+    app.extensions['celery'] = celery
 
     return celery
 
@@ -26,11 +28,13 @@ def create_app(conf_class=None):
     # create and configure application in runtime
 
     app = Flask('app.application')
+
     if conf_class is None:
-        app.config.from_object(f'configs.{os.environ["CONFIG_CLASS"]}')
+        app.config.from_object(f'{CONFIG_MODULE}.{os.environ["CONFIG_CLASS"]}')
     else:
-        app.config.from_object(f'configs.{conf_class}')
-    app.jinja_env.undefined = Config.JINJA_UNDEFINED  # maybe there is a better way to pass this variable
+        app.config.from_object(f'{CONFIG_MODULE}.{conf_class}')
+
+    app.jinja_env.undefined = app.config['JINJA_UNDEFINED']  # maybe there is a better way to pass this variable
 
     # init extensions
     ext.db.init_app(app)
@@ -41,26 +45,32 @@ def create_app(conf_class=None):
     ext.sentry.init_app(app)
     ext.html_min.init_app(app)
     ext.login_manager.init_app(app)
+    create_celery(app, ext.celery)
 
-    celery = create_celery(app, ext.celery)
-    app.extensions['celery'] = celery
+    # logging
+    for handler in app.config['LOGGING_HANDLERS']:
+        handler.setLevel(app.config['LOGGING_LEVEL'])
+        app.logger.addHandler(handler)
 
     # send celery errors
     ext.client.set_dsn(app.config['SENTRY_CONFIG']['dsn'])
     register_logger_signal(ext.client)
     register_signal(ext.client)
-    # TODO check log level
-    register_logger_signal(ext.client, loglevel=app.logger.level)
+    register_logger_signal(ext.client, loglevel=app.config['LOGGING_LEVEL'])
 
     # static slim
-    js = Bundle('js/main.js', filters='jsmin', output='compressed_static/packed.js')
-    css = Bundle('css/main.css', filters='cssmin', output='compressed_static/packed.css')
+    js = Bundle(
+        os.path.join('js', 'main.js'),
+        filters='jsmin',
+        output=os.path.join(app.config['COMPRESS_STATIC_DIR'], 'packed.js'),
+    )
+    css = Bundle(
+        os.path.join('css', 'main.css'),
+        filters='cssmin',
+        output=os.path.join(app.config['COMPRESS_STATIC_DIR'], 'packed.css')
+    )
     ext.assets.register('js_all', js)
     ext.assets.register('css_all', css)
-
-    # TODO get handlers through app
-    for handler in Config.LOGGING_HANDLERS:
-        app.logger.addHandler(handler)
 
     # urls
     from app.account.views import blueprint as account_bp
